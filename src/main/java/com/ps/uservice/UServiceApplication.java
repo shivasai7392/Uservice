7ee737a474b369c6fbaee93e6951829900eb7b89
Shiva Sai Krishna Thota <shivasai7392@gmail.com>
2024-03-15 00:26:13 +0530

    Initial commit

---
+++ src/main/java/com/ps/uservice/UServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ps.uservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ps/uservice/UServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.uservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
